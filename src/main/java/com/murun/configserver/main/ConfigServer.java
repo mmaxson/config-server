95189abe6820913de1d6d0c41ca9e1a02b49c454
mmaxson <mmurun@gmail.com>
2017-01-03 15:03:11 -0800

    encryption

--- src/main/java/com/murun/configserver/main/ConfigServer.java
+++ src/main/java/com/murun/configserver/main/ConfigServer.java
@@ -3,9 +3,12 @@ package com.murun.configserver.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigServer
+@PropertySource("classpath:/properties/application.properties")
+
 public class ConfigServer {
 
     public static void main(String[] arguments) {
